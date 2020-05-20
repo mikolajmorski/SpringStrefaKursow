b678670714879a6a4ad0c6eb08eb0e342936cace
mmorski <19Pokakarotto92>
2020-05-20 23:11:43 +0200

    Initial commit

---
+++ src/main/java/com/morski/springstrefakursow/SpringStrefaKursowApplication.java
@@ -0,0 +1,13 @@
+package com.morski.springstrefakursow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStrefaKursowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStrefaKursowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morski/springstrefakursow/SpringStrefaKursowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morski.springstrefakursow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStrefaKursowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
