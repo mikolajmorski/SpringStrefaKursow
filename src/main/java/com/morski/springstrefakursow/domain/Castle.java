68105b98f2ac4d1ff8d345b066b6574726167ec9
mmorski <19Pokakarotto92>
2020-05-25 22:15:35 +0200

    2. Cykl zycia Spring Beans

--- src/main/java/com/morski/springstrefakursow/domain/Castle.java
+++ src/main/java/com/morski/springstrefakursow/domain/Castle.java
@@ -2,12 +2,24 @@ package com.morski.springstrefakursow.domain;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class Castle {
 
     private String name = "East Watch";
 
     public Castle() {
+    }
 
+    @PostConstruct
+    public void build() {
+        System.out.println("Wybudowano zamek: " + name);
+    }
+
+    @PreDestroy
+    public void tearDown() {
+        System.out.println("Zaraz wyburzumy zamek: " + name);
     }
 }
