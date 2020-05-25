0bd7810bc86b6ebf148a21bc776cdff7d0763c64
mmorski <19Pokakarotto92>
2020-05-25 21:58:41 +0200

    2. Spring Beans, Kontekst, Kontener

---
+++ src/main/java/com/morski/springstrefakursow/domain/Castle.java
@@ -0,0 +1,13 @@
+package com.morski.springstrefakursow.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Castle {
+
+    private String name = "East Watch";
+
+    public Castle() {
+
+    }
+}
