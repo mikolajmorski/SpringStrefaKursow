0975d81561d7968d8359098a48d4061588ca1553
mmorski <19Pokakarotto92>
2020-05-27 21:09:32 +0200

    2. Testowanie aplikacji

--- src/test/java/com/morski/springstrefakursow/SpringStrefaKursowApplicationTests.java
+++ src/test/java/com/morski/springstrefakursow/SpringStrefaKursowApplicationTests.java
@@ -1,13 +1,29 @@
 package com.morski.springstrefakursow;
 
+import com.morski.springstrefakursow.domain.Castle;
+import com.morski.springstrefakursow.domain.Knight;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringStrefaKursowApplicationTests {
 
+	@Autowired
+	Knight knight;
+	@Autowired
+	Castle castle;
+
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	public void testCastle() {
+		String expected = "Znajduje sie tu zamek o nazwie: Castle Black. Zamieszkały przez rycerza: Rycerz o imieniu Lancelot (30). Zadanie: Uratuj ksiezniczke";
+		assertEquals(expected, castle.toString());
+	}
+
 
 }
