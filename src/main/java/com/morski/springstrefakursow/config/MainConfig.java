5f3e22d37403f534a8450f3bcf64579e8c25eb55
mmorski <19Pokakarotto92>
2020-09-30 07:53:14 +0200

    7. Dodanie Spring Security do projektu

--- src/main/java/com/morski/springstrefakursow/config/MainConfig.java
+++ src/main/java/com/morski/springstrefakursow/config/MainConfig.java
@@ -4,9 +4,11 @@ import com.morski.springstrefakursow.domain.repository.DBKnightRepository;
 import com.morski.springstrefakursow.domain.repository.InMemoryKnightRepository;
 import com.morski.springstrefakursow.domain.repository.KnightRepository;
 import net.bytebuddy.build.Plugin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @Configuration
 public class MainConfig {
@@ -24,5 +26,13 @@ public class MainConfig {
         KnightRepository repository = new DBKnightRepository();
         return repository;
     }*/
+
+    @Autowired
+    public void securityUsers(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("user1").roles("USER")
+                .and()
+                .withUser("user2").password("user2").roles("ADMIN");
+    }
 
 }
